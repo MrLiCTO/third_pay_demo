10ea145efe7a202db5ac916f700fbf99f927c5fa
MrLiCTO <javaeemrli@163.com>
2017-02-20 13:31:11 +0800

    注解测试

---
+++ src/main/java/com/lisl/app/beanPost/PeopleAnotation.java
@@ -0,0 +1,17 @@
+package com.lisl.app.beanPost;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2017/2/20.
+ */
+@Target({ElementType.CONSTRUCTOR,ElementType.FIELD,ElementType.METHOD,ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface PeopleAnotation {
+}
